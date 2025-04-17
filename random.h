82de7f99134eabe1e42108b1767b1763bf8eccab
Pedro <pedmarvel@gmail.com>
2025-04-17 20:17:47 +0200

    Delete random.h

--- random.h
+++
@@ -1,8 +0,0 @@
-#ifndef RANDOM
-#define RANDOM
-
-//Precondición: *a >= 1 ; *c >= 'A'
-//Postcondicion: guarda en a y c un num y caracter aleatorios entre 1 y max y entre A y max, formando una coord
-void rand_cord(int *a, char *c, int max);
-
-#endif // RANDOM_H_INCLUDED
