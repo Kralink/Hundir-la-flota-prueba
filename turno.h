a898ff5a4c1db76e7d82d4c559cbbdc06b9d3d72
Pedro <pedmarvel@gmail.com>
2025-04-17 20:18:01 +0200

    Delete turno.h

--- turno.h
+++
@@ -1,9 +0,0 @@
-#ifndef TURNO
-#define TURNO
-#include "estructura.h"
-
-//Precondicion: estructura d inicializada
-//Postcondicon: desarrolla los turnos del juego y pasa el ganador a la funcion "ganador"
-void turno(datos *d);
-
-#endif // TURNO
